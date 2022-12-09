0c6bda2f3ce53512a573fe121cda6d62f9b34b2f
filefromakash <119281182+filefromakash@users.noreply.github.com>
2022-12-09 15:43:15 +0600

    python2 akash.py

---
+++ python2 akash.py
@@ -0,0 +1,10 @@
+# DarkDevill
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install git
+pip2 install requests
+pip2 install mechanize
+git clone https://github.com/filefromakash/gitclone/git
+cd akash
+python2 akash.py
